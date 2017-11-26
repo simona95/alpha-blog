d532a084ea667736a3976e1f8e6945925dbf64e9
Simona <simona.trajceska95@gmail.com>
2017-11-26 22:43:02 +0100

    final commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:update, :edit, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -20,15 +22,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def destroy
+
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -38,7 +47,11 @@ class ArticlesController < ApplicationController
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
+
 end
